56db585f3535c9b39eb549f5c154a039ccdd9fa0
Иван <vania.gorshevhnikov@gmail.com>
2023-04-18 00:02:00 +0300

    Add LogoutTest

--- src/test/java/ru/karpov/NewsPublic/IntegrationTest/User/LogoutTest.java
+++ src/test/java/ru/karpov/NewsPublic/IntegrationTest/User/LogoutTest.java
@@ -4,22 +4,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 import ru.karpov.NewsPublic.IntegrationTest.BaseTest;
+import ru.karpov.NewsPublic.IntegrationTest.Utils;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
-class AuthorizationUserTests extends BaseTest {
-    // Проверяем связь модуля авторизации и остального приложения
-    // Подключаемся через авторизованного пользователя и неавторизованного, проверяем, что система навигации успешно
-    // показывает возможности входа/выхода пользователя
-    @WithMockUser("spring")
+public class LogoutTest extends BaseTest {
     @Test
-    public void authWithCorrectUser() throws Exception {
+    @WithMockUser("Test")
+    public void logoutUser() throws Exception {
+        Utils.createNewUser(userRepo, "Test");
         // Переходим на главную страницу
         MvcResult res = mvc.perform(get("http://localhost:" + port + "/"))
                 .andExpect(status().isOk())
@@ -29,18 +27,29 @@ class AuthorizationUserTests extends BaseTest {
         // Проверяем, что в меню навигации есть кнопка Logout и Profile, что говорит, о том что пользователь аутентифицировался
         Assertions.assertEquals("Logout", document.select("a[href=/logout].me-3.py-2.text-dark.text-decoration-none").text());
         Assertions.assertEquals("Profile", document.select("a[href=/authProfilePage].me-3.py-2.text-dark.text-decoration-none").text());
-    }
-
-    @Test
-    public void authWithAnonymousUser() throws Exception {
-        MvcResult res = mvc.perform(get("http://localhost:" + port + "/"))
+        res = mvc.perform(get("http://localhost:" + port + "/logout"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("mainPage"))
                 .andReturn();
-        Document document = Jsoup.parse(res.getResponse().getContentAsString());
+        document = Jsoup.parse(res.getResponse().getContentAsString());
         // Проверяем, что в меню навигации есть кнопка LogIn и нет кнопки Logout, что говорит, о том что пользователь НЕ аутентифицирован
         Assertions.assertEquals(0, document.select("a[href=/logout].me-3.py-2.text-dark.text-decoration-none").size());
         Assertions.assertEquals("LogIn", document.select("a[href=/authProfilePage].me-3.py-2.text-dark.text-decoration-none").text());
+        // Проверяем, что при нажатии на кнопки модуля навигации, на всех, кроме главной страницы, нас перенаправляет на /sso/login
+        mvc.perform(get("http://localhost:" + port + "/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("mainPage"));
+
+        mvc.perform(get("http://localhost:" + port + "/addNewsPage"))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("/sso/login"));
+
+        mvc.perform(get("http://localhost:" + port + "/subscriptionsPage"))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("/sso/login"));
+
+        mvc.perform(get("http://localhost:" + port + "/authProfilePage"))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("/sso/login"));
     }
 }
-
